b23648f420f0736a159cd185e6fff2e1e32fd8a3
JW05 <jens.wouters.18@gmail.com>
2020-03-25 15:46:27 +0100

    UpdateProfile.php bijgewerkt

---
+++ UpdateProfile.php
@@ -0,0 +1,73 @@
+<h1>Vervoledig hier je profiel</h1>
+
+
+<fieldset>
+<legend><h1>Kenmerken</h1></legend>
+<form action="/action_page.php">
+  <label for="locatie">Van waar bent u?</label><br>
+  <input type="text" id="locatie" name="locatie" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+<h2>In welk jaar zit u</h2>
+
+<form action="/action_page.php">
+  <label for="jaar"></label>
+  <select id="jaar" name="jaar">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+  </select>
+  <input type="submit">
+</form>
+
+
+<h2>Verkiest u development of design?</h2>
+
+<form action="/action_page.php">
+  <label for="voorkeur"></label>
+  <select id="voorkeur" name="voorkeur">
+    <option value="Development">Development</option>
+    <option value="Design">Design</option>
+  </select>
+  <input type="submit">
+</form>
+
+
+
+
+<h2>Wat is je muziek genre?</h2>
+
+<form action="/action_page.php">
+  <label for="genre"></label>
+  <select id="genre" name="genre">
+    <option value="Pop">Pop</option>
+    <option value="Rock">Rock</option>
+    <option value="R&B">R&B</option>
+    <option value="Latin">Latin</option>
+    <option value="Electronische muziek">Electronische muziek</option>
+    <option value="Drum-'n-bass">Drum-'n-bass</option>
+    <option value="Klasiek">Klasiek</option>
+  </select>
+  <input type="submit">
+</form>
+
+
+
+<h2>Bent u een feestbeest?</h2>
+
+<form action="/action_page.php">
+  <label for="feesten"></label>
+  <select id="feesten" name="feesten">
+    <option value="Ja">Ja</option>
+    <option value="Nee">Nee</option>
+  </select>
+  <input type="submit">
+</form>
+
+</fieldset>
+
+
+
+
