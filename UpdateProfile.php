1f1b699cc9343c68f41d661fec3f0330cab7793e
JW05 <jens.wouters.18@gmail.com>
2020-03-25 16:46:47 +0100

    UpdateProfile.php forms updatet

--- UpdateProfile.php
+++ UpdateProfile.php
@@ -6,40 +6,38 @@
 <form action="/PHP-project-buddy/PHP-project-buddy/ProfilePage.php">
   <label for="locatie">Van waar bent u?</label><br>
   <input type="text" id="locatie" name="locatie" value=""><br><br>
-  <input type="submit" value="Submit">
-</form> 
+  
+ 
 
 
 <h2>In welk jaar zit u</h2>
 
-<form action="/PHP-project-buddy/PHP-project-buddy/ProfilePage.php">
+
   <label for="jaar"></label>
   <select id="jaar" name="jaar">
     <option value="1">1</option>
     <option value="2">2</option>
     <option value="3">3</option>
   </select>
-  <input type="submit">
-</form>
+  
 
 
 <h2>Verkiest u development of design?</h2>
 
-<form action="/PHP-project-buddy/PHP-project-buddy/ProfilePage.php">
+
   <label for="voorkeur"></label>
   <select id="voorkeur" name="voorkeur">
     <option value="Development">Development</option>
     <option value="Design">Design</option>
   </select>
-  <input type="submit">
-</form>
+ 
 
 
 
 
 <h2>Wat is je muziek genre?</h2>
 
-<form action="/PHP-project-buddy/PHP-project-buddy/ProfilePage.php">
+
   <label for="genre"></label>
   <select id="genre" name="genre">
     <option value="Pop">Pop</option>
@@ -50,19 +48,18 @@
     <option value="Drum-'n-bass">Drum-'n-bass</option>
     <option value="Klasiek">Klasiek</option>
   </select>
-  <input type="submit">
-</form>
+  
 
 
 
 <h2>Bent u een feestbeest?</h2>
 
-<form action="/PHP-project-buddy/PHP-project-buddy/ProfilePage.php">
+
   <label for="feesten"></label>
   <select id="feesten" name="feesten">
     <option value="Ja">Ja</option>
     <option value="Nee">Nee</option>
-  </select>
+  </select><br><br>
   <input type="submit">
 </form>
 
