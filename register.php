6c1e6e302b2fe6ad7d89bae3bdd85c8c3d3fd266
Bryan Van Mechelen <bryan.van.mechelen@hotmail.com>
2020-03-29 19:15:24 +0200

    Added register HTML and NewUser class with getters and setters

---
+++ register.php
@@ -0,0 +1,38 @@
+<?php
+	include_once(__DIR__."/classes/newUser.php");
+	 /* een account aanmaken kan met
+    Email
+    Dit adres moet eindigen op @student.thomasmore.be
+    Dit adres mag nog niet bestaan, dubbele accounts aanmaken mag dus niet mogelijk zijn, toon een fout als het email adres reeds in gebruik is
+    Full name
+    password (veilig bewaard via bcrypt!)
+    zorg voor een foutmelding indien het aanmaken van een account niet lukt 
+	valideer al wat kan mislopen in dit formulier via PHP en toon gebruiksvriendelijke foutmeldingen */
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="register">
+			<h1>Register</h1>
+			<form action="" method="post">
+                <div class="error"></div>
+
+				<input type="text" name="firstName" placeholder="First name" id="firstName" required>
+
+                <input type="text" name="lastName" placeholder="Last name" id="lastName" required>
+
+				<input type="password" name="password" placeholder="Password" id="password" required>
+
+				<input type="email" name="email" placeholder="Email" id="email" required>
+
+				<input type="submit" value="Register">
+			</form>
+		</div>
+</body>
+</html>
