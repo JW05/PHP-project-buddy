585239830d261f54f67be4880b2f095ab25ee714
MauryMassa <maury_massa@outlook.com>
2020-04-04 16:18:15 +0100

    profilepage omzetting

--- ProfilePage.php
+++ ProfilePage.php
@@ -10,45 +10,45 @@
         $currentKenmerk = new User();
 
         $currentKenmerk->setUserId($_POST["userId"]);
-        $currentKenmerk->setLocatie($_POST["locatie"]);
-        $currentKenmerk->setJaar($_POST["jaar"]);
-        $currentKenmerk->setVoorkeur($_POST["voorkeur"]);
+        $currentKenmerk->setLocatie($_POST["location"]);
+        $currentKenmerk->setJaar($_POST["year"]);
+        $currentKenmerk->setVoorkeur($_POST["preference"]);
         $currentKenmerk->setGenre($_POST["genre"]);
-        $currentKenmerk->setFeesten($_POST["feesten"]);
+        $currentKenmerk->setFeesten($_POST["likestoparty"]);
         
 
         if(!empty($_POST)){
          $userId = $_POST['userId'];   
-         $locatie = $_POST['locatie'];
-         $jaar = $_POST['jaar'];
-         $voorkeur = $_POST['voorkeur'];
+         $locatie = $_POST['location'];
+         $jaar = $_POST['year'];
+         $voorkeur = $_POST['prefernce'];
          $genre = $_POST['genre'];
-         $feesten = $_POST['feesten'];
+         $feesten = $_POST['likestoparty'];
    
          
 if(!empty($userId)){
     $currentKenmerk->setUserId(htmlspecialchars($userId));
 }
 if(!empty($locatie)){
-    $currentKenmerk->setLocatie(htmlspecialchars($locatie));
+    $currentKenmerk->setLocation(htmlspecialchars($location));
 }else{
     $error = "Please fill out all the fields.";
 }
 
-if(!empty($jaar)){
-    $currentKenmerk->setJaar(htmlspecialchars($jaar));
+if(!empty($year)){
+    $currentKenmerk->setYear(htmlspecialchars($year));
 }
 
-if(!empty($voorkeur)){
-    $currentKenmerk->setVoorkeur(htmlspecialchars($voorkeur));
+if(!empty($preference)){
+    $currentKenmerk->setPreference(htmlspecialchars($preference));
 }
 
 if(!empty($genre)){
     $currentKenmerk->setGenre(htmlspecialchars($genre));
 }
 
-if(!empty($feesten)){
-    $currentKenmerk->setFeesten(htmlspecialchars($feesten));
+if(!empty($likestoparty)){
+    $currentKenmerk->setParty(htmlspecialchars($likestoparty));
 }
 
 }
@@ -78,15 +78,21 @@ if(!empty($feesten)){
     
     
     <legend><h1>Kenmerken</h1></legend>
-    <h2>Locatie: <?php echo $currentKenmerk->getLocatie();?></h2>
-    <h2>Jaar: <?php echo $currentKenmerk->getJaar();?></h2>
-    <h2>Voorkeur: <?php echo $currentKenmerk->getVoorkeur(); ?></h2>
-    <h2>Muziek genre: <?php echo $currentKenmerk->getGenre(); ?></h2>
-    <h2>Feestbeest: <?php echo $currentKenmerk->getFeesten(); ?></h2>
+    <h2>Location: <?php echo $currentKenmerk->getLocatie();?></h2>
+    <h2>Year: <?php echo $currentKenmerk->getJaar();?></h2>
+    <h2>Preference: <?php echo $currentKenmerk->getVoorkeur(); ?></h2>
+    <h2>Music genre: <?php echo $currentKenmerk->getGenre(); ?></h2>
+    <h2>Party animal: <?php echo $currentKenmerk->getFeesten(); ?></h2>
     <h2>userId: <?php echo $currentKenmerk->getUserId(); ?></h2>
 
 
-    
+    Buddy:
+<input type="radio" name="buddy"
+<?php if (isset($buddy) && $buddy=="buddy") echo "checked";?>
+value="looking for">Looking for a Buddy
+<input type="radio" name="buddy"
+<?php if (isset($buddy) && $buddy=="male") echo "checked";?>
+value="accepting">Guarding over a Buddy
    
 </div>
 </fieldset>
