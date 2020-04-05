d46a6bed8f30e6e6c2e6ea90ffcdec0440e610dd
JW05 <jens.wouters.18@gmail.com>
2020-04-05 10:07:43 +0200

    update user.php

--- classes/User.php
+++ classes/User.php
@@ -321,46 +321,57 @@ public function saveBuddy()
 
 
 
-   /* public function updateUserId($id){
-        $conn = Db::getConnection();
+  
+
+
+
+/* -----------------------------------------------------------------------------*/
+
+/* check if id exists in profile table to execute insert Updateprofile data */
+
+public function UserIdExists($id)
+{
+        $conn = new mysqli("localhost", "root", "","phpals");
+        $query="select * from profile where userId ='$id'";
+        $result = $conn->query($query);
+        if(mysqli_num_rows($result)!=0)
        
-       $statement = $conn->prepare("insert into user (id) values (:id)"); 
-
-        
-        $id = $this->getId();
-        
-        $statement->bindValue(":id", $id);
-
-        $result = $statement->execute();
-
-            return true;
-             $_SESSION['locatie'] = "phpals.profile";
-}*/
-
- /**
-                 * Get the value of id
-                 */ 
-           /*     public function getId()
-                {
-                                return $this->id;
+         {      
+                echo "true user id exists !!! ";
+                return true;}
+           else	{
+                echo "false user id doesnt exist zzzzzzzzzz";
+                return false;
                 }
+}
 
+public function getUserId2($email)
+{
+        $conn = new mysqli("localhost", "root", "","phpals");
+        $email = $conn->real_escape_string($email);
+        $query="select id from users where email = '$email'";
+        $result = $conn->query($query);
+        
+        if(mysqli_num_rows($result)!=0)
+        {
+                        $user = $result->fetch_assoc();
+                        return $user;
+        
+               }else
+                {
+                        return false;
+                }
+        }   
+        
+ //--------------------------end check if id exists in profile table to execute insert Updateprofile data-----//        
                 /**
-                 * Set the value of id
-                 *
-                 * @return  self
+                 * Get the value of userId
                  */ 
-          /*      public function setId($id)
+
+                public function getUserId()
                 {
-                                $this->id = $id;
-
-                                return $this;
+                                return $this->userId;
                 }
-
-/* END NO USE ------------------------------------------------------------------------*/
-
-
-
                   /**
                  * Get the value of userId
                  */ 
@@ -493,50 +504,22 @@ public function saveBuddy()
 	
 /*--JW------------------------------BuddyPage------------------------------------------------------*/
 
-/* lookup function for the user id of the entered email or return not found */ 
-                public function getUserId2($email)
-                {
-                        $conn = new mysqli("localhost", "root", "","phpals");
-                        $email = $conn->real_escape_string($email);
-                        $query="select id from users where email = '$email'";
-                        $result = $conn->query($query);
-                        
-                        if(mysqli_num_rows($result)!=0)
-                        {
-                                        $user = $result->fetch_assoc();
-                                        return $user;
-                        
-                               }else
-                                {
-                                        return false;
-                                }
+public function getBuddys($userId) {					
+        $conn = new mysqli("localhost", "root", "","phpals");
+        $userId = $conn->real_escape_string($userId);
+        $query="select u.firstname, u.lastname, u.avatar from users u left join buddys b on u.id = b.buddyid where b.userid = '$userId'";
+        $result = $conn->query($query);
+        if(mysqli_num_rows($result)!=0) {
+                        //place all data in the array row to be able to use 
+                        while($row = $result->fetch_assoc()) {
+                                $rows[] = $row;
                         }
-                
-               
-                public static function getBuddys($userid){
-                        $conn = Db::getConnection();
-                                              
-                        $statement = $conn->prepare("select buddyId from buddys where userId = $userid ");
-                        $statement->execute(); 
-                        $buddys = $statement->fetchAll(PDO::FETCH_ASSOC);
-                
-                        return $buddys;
-                
+                        return $rows;   
+                }else {
+                        return false;
                 }
-
-/* No use at the moment just test */
-                public function  AllBuddys($id){
-                        $conn = Db::getConnection();
-                        $query="select * from buddys where id ='$id'";
-
-                }
-/* end no use ---------------------*/
-
-
-
-
-
-
+} 
+ 
 
    }
 ?>
