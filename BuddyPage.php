c8ea06c721b573b5c14d0a8c5f7aed120064ce38
MauryMassa <maury_massa@outlook.com>
2020-04-05 15:30:31 +0100

    links toegevoegd + andere veranderingen

--- BuddyPage.php
+++ BuddyPage.php
@@ -19,6 +19,9 @@ include_once(__DIR__."/classes/User.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BuddyPage</title>
