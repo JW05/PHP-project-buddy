277db0c256655c1c121d9f50520f48c938c2bac5
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-04-06 01:21:09 +0200

    Removed useless files and changed name of updateProfile to camelCase.

--- UpdateProfile.php
+++ UpdateProfile.php
@@ -1,28 +1,32 @@
 <?php
-// JENS UPDATE 
-include_once(__DIR__."/classes/User.php");
-session_start();
+  // JENS UPDATE 
+  include_once(__DIR__."/classes/User.php");
+  session_start();
 
+  try{
     $user = User::getCurrentUser($_SESSION['user']);
     $userId = $user['id'];
+    $currentPreference = User::getCurrentPreference($userId);
+  }catch(\Throwable $th){
+    $error = $th->getMessage();
+  }
     
-if(!empty($_POST)){
+  if(!empty($_POST)){
   
-  try {
-         $info = new User();
-         $info->setLocation ($_POST['location']);
-         $info->setYear ($_POST['year']);
-         $info->setPreference ($_POST['preference']);
-         $info->setGenre ($_POST['genre']);
-         $info->setLikesToParty ($_POST['likesToParty']);
-         $info->setLookingForBuddy ($_POST['lookingForBuddy']);
-         $info->setUserId ($userId); 
+    try {
+      $info = new User();
+      $info->setLocation ($_POST['location']);
+      $info->setYear ($_POST['year']);
+      $info->setPreference ($_POST['preference']);
+      $info->setGenre ($_POST['genre']);
+      $info->setLikesToParty ($_POST['likesToParty']);
+      $info->setLookingForBuddy ($_POST['lookingForBuddy']);
 
-         $result = $info->updateInfo();
-         // If the result from the save is success, redirect to the index.
-         if($result) {
-          header('Location: index.php');
-         }
+      $result = $info->updateInfo($userId);
+      // If the result from the save is success, redirect to the index.
+      if(!empty($result)) {
+        $success = "Preferences are up to date";
+      }
 
   } catch (\Throwable $th) {
     $error =$th->getMessage();
@@ -55,91 +59,73 @@ if(!empty($_POST)){
 </head>
 <body>
 
- <?php if(isset($error)):?>
-  <div class="error"><?php echo $error; ?></div>
-  <?php endif; ?>
-  <?php if(isset($succes)):?>
-  <div class="succes"><?php echo $succes; ?></div>
-  <?php endif; ?>
 
+  <h1>Vervoledig hier je profiel</h1>
+  <br><br>
+  <legend><h1>Kenmerken</h1></legend>
+  <form action="" method="post">
+    <?php if(isset($error)):?>
+      <div class="error"><?php echo $error; ?></div>
+    <?php endif; ?>
+    <?php if(isset($success)):?>
+      <div class="succes"><?php echo $success; ?></div>
+    <?php endif; ?>
 
-<h1>Vervoledig hier je profiel</h1>
-<br><br>
-<legend><h1>Kenmerken</h1></legend>
-<form action="" method="post">
-
-  <div>
-  <label for="location"><h2>Van waar bent u?</h2></label><br>
-  <input type="text" id="location" name="location" value=""><br><br>
-  </div>
+    <div>
+      <label for="location"><h2>Van waar bent u?</h2></label><br>
+      <input type="text" id="location" name="location" value="<?php echo htmlspecialchars($currentPreference->location);?>"><br><br>
+    </div>
  
-<div>
-<h2>In welk jaar zit u</h2>
+    <div>
+      <label for="year"><h2>In welk jaar zit u</h2></label>
+      <select id="year" name="year">
+        <option value="1" <?php echo ($currentPreference->year == 1)? "selected": ""; ?>>1</option>
+        <option value="2" <?php echo ($currentPreference->year == 2)? "selected": ""; ?>>2</option>
+        <option value="3" <?php echo ($currentPreference->year == 3)? "selected": ""; ?>>3</option>
+      </select>
+    </div> 
 
-  <label for="year"></label>
-  <select id="year" name="year">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-  </select>
- </div> 
+    <div>
+      <label for="preference"><h2>Verkiest u development of design?</h2></label>
+      <select id="preference" name="preference">
+        <option value="Development" <?php echo ($currentPreference->preference == "Development")? "selected": ""; ?>>Development</option>
+        <option value="Design" <?php echo ($currentPreference->preference == "Design")? "selected": ""; ?>>Design</option>
+      </select>
+    </div>
 
-<div>
-<h2>Verkiest u development of design?</h2>
+    <div>
+      <label for="genre"><h2>What is your music genre?</h2></label>
+      <select id="genre" name="genre">
+        <option value="Pop" <?php echo ($currentPreference->genre == "Pop")? "selected": ""; ?>>Pop</option>
+        <option value="Rock" <?php echo ($currentPreference->genre == "Rock")? "selected": ""; ?>>Rock</option>
+        <option value="R&B" <?php echo ($currentPreference->genre == "R&B")? "selected": ""; ?>>R&B</option>
+        <option value="Latin" <?php echo ($currentPreference->genre == "Latin")? "selected": ""; ?>>Latin</option>
+        <option value="Electronische Music" <?php echo ($currentPreference->genre == "Electronische Music")? "selected": ""; ?>>Electronic Muzic</option>
+        <option value="Drum-'n-bass" <?php echo ($currentPreference->genre == "Drum-'n-bass")? "selected": ""; ?>>Drum-'n-bass</option>
+        <option value="Classic" <?php echo ($currentPreference->genre == "Classic")? "selected": ""; ?>>Classic</option>
+      </select>
+    </div>  
 
-  <label for="preference"></label>
-  <select id="preference" name="preference">
-    <option value="Development">Development</option>
-    <option value="Design">Design</option>
-  </select>
- </div>
+    <div>
+      <label for="likesToParty"><h2>Party animal?</h2></label>
+      <select id="likesToParty" name="likesToParty">
+        <option value="1" <?php echo ($currentPreference->likesToParty == 1)? "selected": ""; ?>>Ja</option>
+        <option value="0" <?php echo ($currentPreference->likesToParty == 0)? "selected": ""; ?>>Nee</option>
+      </select><br><br>
+    </div>
 
-<div>
-<h2>What is your music genre?</h2>
+    <div>
+      <label for="lookingForBuddy"><h2>Need a buddy?</h2></label>
+      <select id="lookingForBuddy" name="lookingForBuddy">
+        <option value="1" <?php echo ($currentPreference->lookingForBuddy == 1)? "selected": ""; ?>>I am looking for a buddy</option>
+        <option value="0" <?php echo ($currentPreference->lookingForBuddy == 0)? "selected": ""; ?>>I would like to take care of someone</option>
+      </select><br><br>
+    </div>
 
-  <label for="genre"></label>
-  <select id="genre" name="genre">
-    <option value="Pop">Pop</option>
-    <option value="Rock">Rock</option>
-    <option value="R&B">R&B</option>
-    <option value="Latin">Latin</option>
-    <option value="Electronische Music">Electronic Muzic</option>
-    <option value="Drum-'n-bass">Drum-'n-bass</option>
-    <option value="Classic">Classic</option>
-  </select>
-</div>  
-
-<div>
-<h2>Party animal?</h2>
-
-  <label for="likesToParty"></label>
-  <select id="likesToParty" name="likesToParty">
-    <option value="1">Ja</option>
-    <option value="0">Nee</option>
-  </select><br><br>
-</div>
-
-
-
-
-<h2>Need a buddy?</h2>
-
-  <label for="lookingForBuddy"></label>
-  <select id="lookingForBuddy" name="lookingForBuddy">
-    <option value="0">I am looking for a buddy</option>
-    <option value="1">I would like to take care of someone</option>
-  </select><br><br>
-</div>
-
-   
-
-
-
-
-<div>
-  <input class = "button" type="submit">
-</div>
-</form>
+    <div>
+      <input class = "button" type="submit">
+    </div>
+  </form>
 
 </body>
 </html>
