ffc01642da6b96ec724dc10b492d1f84b7b6320b
vmbryan <55796603+vmbryan@users.noreply.github.com>
2020-04-08 21:12:40 +0200

    Added search functionality for preference and genre in seperate search class. Also edited index.php to support search functionality

--- index.php
+++ index.php
@@ -9,6 +9,25 @@
        // header("Location: insertProfile.php");
     }
 
+
+    //SEARCH BRYAN
+    include_once(__DIR__ . "/classes/Search.php");
+
+    $searchReturned = null;
+
+    if(!empty($_POST['search'])){
+        try {
+            //code...
+            $search = new Search();
+            $search->setSearch($_POST['search']);
+            $searchReturned = $search->getData();
+        } catch (\Throwable $th) {
+            //throw $th;
+            $error = $th->getMessage();
+        }
+    }
+
+    //END SEARCH
 ?>
 
 <!DOCTYPE html>
@@ -18,20 +37,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHPals</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
 </head>
 <body>
     
     <a href="ProfilePage.php"> Profile </a>
 
     <a href="logout.php" class="btn-logout">logout</a>
+
+    <!-- SEARCH BRYAN -->
     <div class="search-container">
-        <form action="index.php" method="POST"></form>
+        <form action="index.php" method="POST">
         <h1>Search for a buddy by interest or name here</h1>
         <input type="text" name="search" placeholder="name or interest">
         <button type="submit" name="search-action">Search</button>
+        </form>
     </div>
 
+    <?php
+    if($searchReturned != null){
+        foreach($searchReturned as $result){
+    ?>
+    <div>
+        <h1><?php echo $result["firstname"] . " " . $result["lastname"]?></h1>
+    </div>
+    <?php }} ?>
+    <!-- END SEARCH BRYAN -->
+
+    
     <div class="side-bar">
         <div class="same-intrest">
             <h2>These people have the same interests</h2>
