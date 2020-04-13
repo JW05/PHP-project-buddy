1642f621b3e2ce8a050aed3f5b9ec6df6a5a78a2
JW05 <jens.wouters.18@gmail.com>
2020-04-13 17:34:58 +0200

    feature13: Circle.css,Circle.js(+map js) added voor opmaak (animatie werkt nog niet)

--- index.php
+++ index.php
@@ -38,9 +38,10 @@
     <title>PHPals</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/Circle.css">
 </head>
 <body>
-    
+    <script src="js/Circle.js"></script>
     <?php
         include_once(__DIR__."/nav.inc.php");
     ?>
@@ -74,26 +75,56 @@
         
         </div>
     </div>
-    <!-- visual geregistreerde studenten -->
+    <!-- including geregistreerde studenten + buddy overeenkomsten-->
     <?php    
            $AllStudents = User::getAllStudents();
     ?>
-           
-          
-           <?php foreach($AllStudents as $student): ?>
-		   <h2><?php echo $student; ?></h2>
-           <?php endforeach; ?>
-
-    <!-- visual buddy overeenkomsten -->
     <?php
            $AllBuddys = User::getAllBuddys();
     ?>
-           
-          
+        
 
-           <?php foreach($AllBuddys as $Buddy): ?>
-		   <h2><?php echo $Buddy; ?></h2>
-           <?php endforeach; ?>
+<ul class="display-container">
+ <li class="note-display" data-note="7.50">
+    <div class="circle">
+      <svg width="84" height="84" class="circle__svg">
+        <circle cx="41" cy="41" r="38" class="circle__progress circle__progress--path"></circle>
+        <circle cx="41" cy="41" r="38" class="circle__progress circle__progress--fill"></circle>
+      </svg>
+
+      <div class="percent">
+        <span class="percent__int">
+        <?php foreach($AllStudents as $student): ?>
+        <h2><?php echo $student; ?></h2>
+        <?php endforeach; ?>
+        </span>
+       <!-- <span class="percent__dec">00</span> -->
+      </div>
+    </div>
+
+    <span class="label">Students</span>
+  </li>
+
+ <li class="note-display" data-note="9.27">
+    <div class="circle">
+      <svg width="84" height="84" class="circle__svg">
+        <circle cx="41" cy="41" r="38" class="circle__progress circle__progress--path"></circle>
+        <circle cx="41" cy="41" r="38" class="circle__progress circle__progress--fill"></circle>
+      </svg>
+
+      <div class="percent">
+        <span class="percent__int">
+        <?php foreach($AllBuddys as $Buddy): ?>
+        <h2><?php echo $Buddy; ?></h2>
+        <?php endforeach; ?>
+        </span>
+      <!--  <span class="percent__dec">00</span> -->
+      </div>
+    </div>
+
+    <span class="label">Buddys</span>
+  </li>
+</ul>
 
 
 </body>
