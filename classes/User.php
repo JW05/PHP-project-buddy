24bb716d8ca1a4a772652699a8195b023813fdf4
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-05-02 16:37:14 +0200

    Feature 2: forgot to change line 226 to a PDO function

--- classes/User.php
+++ classes/User.php
@@ -223,7 +223,7 @@
 
                         $result = $statement->execute();
                         
-			if(mysqli_num_rows($result)!=0)
+			if($result->fetchColumn() > 0)
 			{
 				$user = $statement->fetch(PDO::FETCH_ASSOC);
 				if(password_verify($password,$user['password'])){
