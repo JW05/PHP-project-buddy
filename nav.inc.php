b679ab936c1eb624cb3ab432ec4feb00ac1763a0
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-05-08 20:38:16 +0200

    Layout of navigation fixed + made mobile free

--- nav.inc.php
+++ nav.inc.php
@@ -6,26 +6,76 @@
   $notifsBySender = Message::getUnreadNotifBySender($user['id']);
 
   $ammountNotif = count($notifsBySender);
+
+  //SEARCH BRYAN
+  include_once(__DIR__ . "/classes/Search.php");
+
+  $searchReturned = null;
+
+  if(!empty($_POST['name']) || !empty($_POST['preferences']) || !empty($_POST['genres'])){
+      try {
+          //code...
+          $search = new Search();
+          $search->setName($_POST['name']);
+          $search->setPreference($_POST['preferences']);
+          $search->setGenre($_POST['genres']);
+          $searchReturned = $search->getData();
+      } catch (\Throwable $th) {
+          //throw $th;
+          $error = $th->getMessage();
+      }
+  }
+
+  //END SEARCH
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
   <a class="navbar-brand" href="#"><img src="img/Untitled-1.png" width="100" height="auto" alt="logo"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <!-- SEARCH BRYAN -->
+    <div class="search-container">
+        <form action="index.php" method="POST" id="searchform">
+          <div class="d-flex flex-row">
+            <label for="preferences" id="prefLabel">Preference</label>
+            <select name="preferences" id="preferences" form="searchform">
+              <option value=""></option>
+              <option value="design">Design</option>
+              <option value="development">Development</option>
+            </select>
+          </div>
+
+          <div class="d-flex flex-row"> 
+            <label for="genres" id="genresLabel">Genre</label>
+            <select name="genres" id="genres" form="searchform">
+              <option value=""></option>
+              <option value="Pop">Pop</option>
+              <option value="Rock">Rock</option>
+              <option value="R&B">R&B</option>
+              <option value="Latin">Latin</option>
+              <option value="Drum-'n-bass">Drum-'n-bass</option>
+              <option value="classic">Classic</option>
+            </select>
+          </div>
+          <div class="d-flex flex-row"> 
+            <label for="name" id="nameLabel">Name</label>
+            <input type="text" name="name" placeholder="name" id="name">
+          </div>
+          <input type="submit" name="search-action" value="Search" id="searchBtn"></input>
+
+        </form>
+    </div>
+    <!-- END SEARCH BRYAN -->
+    <ul class="navbar-nav">
       <li class="nav-item <?php echo ($activePage == "index")? "active":""; ?>">
         <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item <?php echo ($activePage == "profilePage")? "active":""; ?>">
         <a class="nav-link" href="profilePage.php">My Profile</a>
       </li>
-      <li class="nav-item  <?php echo ($activePage == "profileSetting")? "active":""; ?>">
-        <a class="nav-link" href="profileSetting.php">My Account</a>
-      </li>
-
       <li class="nav-item  <?php echo ($activePage == "FAQ")? "active":""; ?>">
         <a class="nav-link" href="FAQ.php">FAQ</a>
       </li>
@@ -46,8 +96,6 @@
         </div>
             <?php endif; ?>
       </li>
-    </ul>
-    <ul class="navbar-nav float-right">
       <li class="nav-item logout">
         <a class="nav-link" href="logout.php" id="white">Logout</a>
       </li>
