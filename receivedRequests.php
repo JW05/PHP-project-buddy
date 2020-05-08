6a938339d668cb04e2ee5346fd905876d4e35728
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-05-09 00:30:16 +0200

    Made layout of index.php responsive

--- receivedRequests.php
+++ receivedRequests.php
@@ -23,33 +23,33 @@
   }
 ?>
 
-
-
-
 <div class="row">
 
 <?php foreach($receivedRequest as $request):
   $senderUser = User::getUserInfo($request['userId']); ?>
-  <div class="card col-md-4" style="width: 3rem;">
-
-    <img src="img/avatar/<?php echo htmlspecialchars($senderUser->avatar);?>" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo htmlspecialchars($senderUser->firstname)." ".htmlspecialchars($senderUser->lastname);?></h5>
- 
-      <form  action="" method="post">
-        <input type="hidden" id="buddyId" name="buddyId" value="<?php echo $request['userId']; ?>">
-
-        <label for="acceptRequest">Accept request</label>
-        <select id="acceptRequest" name="requestAccepted">
-          <option value="1">Accept match</option>
-          <option value="0">Decline match</option>
-        </select><br>
-
-        <label for="reasonDenial">Reason</label>
-        <textarea name="reasonDenial" id="reasonDenial" cols="30" rows="2" placeholder="Reason for dislike"></textarea>
-        <input class = "button" type="submit" name="requestAction" placeholder="send">
-      </form>
- 
+  <div class="col-12 p-1">
+    <div class="card">
+      <div class="card-body">
+      <img src="img/avatar/<?php echo htmlspecialchars($senderUser->avatar);?>" class="card-img-top rounded-circle mx-auto d-block" alt="...">
+        <h5 class="card-title text-center"><?php echo htmlspecialchars($senderUser->firstname)." ".htmlspecialchars($senderUser->lastname);?></h5>
+  
+        <form  action="" method="post" id="requestForm">
+          <input type="hidden" id="buddyId" name="buddyId" value="<?php echo $request['userId']; ?>">
+          <div class="form-group d-flex flex-column text-left">
+            <label for="acceptRequest">Accept request</label>
+            <select id="acceptRequest" name="requestAccepted">
+              <option value="1">Accept match</option>
+              <option value="0">Decline match</option>
+            </select>
+          </div>
+          <div class="form-group text-left">
+            <label for="reasonDenial">Reason</label>
+            <textarea name="reasonDenial" id="reasonDenial" cols="30" rows="2" placeholder="Reason for dislike"></textarea>
+          </div>
+          <input class = "button" type="submit" name="requestAction" placeholder="send">
+        </form>
+  
+      </div>
     </div>
   </div>
 <?php endforeach;?>
